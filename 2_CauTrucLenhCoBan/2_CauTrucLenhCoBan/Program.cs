8f0cfc310950548d225ed0eb85fbc1d154f8c03f
thien47 <nguyenthien191200@gmail.com>
2025-01-17 00:26:03 +0700

    feat: Basic command structure in C#

---
+++ 2_CauTrucLenhCoBan/2_CauTrucLenhCoBan/Program.cs
@@ -0,0 +1,21 @@
+﻿//Thư viện hỗ trợ nhập xuất dữ liệu
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace chứa các thành phần bên trong bao gồm các class, enum hoặc namespace con
+namespace Cau_Truc_Lenh_Co_Ban
+{
+    // báo cho trình biên dịch biết rằng những thành phần trong khối { } ngay sau tên lớp thuộc vào chính class đó.
+    class Program
+    {
+        //Hàm chính của toàn chương trình. Mỗi khi trình biên dịch dịch chương trình ra sẽ đi vào hàm Main đầu tiên để bắt đầu vòng đời của chương trình.
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello World!");// Chương trình
+        }  
+    }
+}
+
